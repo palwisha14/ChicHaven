92bb127926a8601b8e8aa726ab96b771a03c086c
palwisha ilyas <107913603+palwisha14@users.noreply.github.com>
2025-08-03 01:36:51 +0500

    'firstcommit'

---
+++ front/script/script.js
@@ -0,0 +1,19 @@
+
+// =====================
+//  navbar
+// =====================
+$(".bar").click(function () {
+  $(".nav1").slideToggle();
+});
+
+// =====================
+//  Testimonials
+// =====================
+$('.owl-carousel').owlCarousel({
+  items:1,
+  loop:true,
+  margin:10,
+  nav:true,
+  dots:false,
+  
+})
